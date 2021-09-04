2ae297acd9bfb76a8e3e58d7f922527447a3f8ca
gebo <gebogebogeboge@gmail.com>
2021-09-05 07:24:04 +0900

    update

---
+++ spring-security-adapter/src/main/kotlin/com/example/springsecurityadapter/SpringSecurityAdapterApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springsecurityadapter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringSecurityAdapterApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringSecurityAdapterApplication>(*args)
+}
---
+++ spring-security-adapter/src/test/kotlin/com/example/springsecurityadapter/SpringSecurityAdapterApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springsecurityadapter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringSecurityAdapterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
